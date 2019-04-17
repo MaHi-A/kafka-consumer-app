dfb4ef2db38f61012de531575f9c2a4293975eba
Maheshwar Rao Anathela <manathela@Lumeras-MacBook-Pro.local>
2019-04-17 15:42:51 +0530

    Refactoring the names

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,9 @@ class WelcomeController < ActionController::Base
 	before_action :initialize_kafka
 
 	def show
-		response = []
-		@kafka.each_message(topic: 'greetings') { |message| response.push({ offset: message.offset, key: message.key, value: message.value }) }
-		render json: response
+		messages = []
+		@kafka.each_message(topic: 'greetings') { |message| messages.push({ offset: message.offset, key: message.key, value: message.value }) }
+		render json: messages
 	end
 
 	private
